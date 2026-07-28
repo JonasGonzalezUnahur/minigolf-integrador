9a60804d6447824a97f94261aaab952197b899d1
JonasGonzalezUnahur <166155934+JonasGonzalezUnahur@users.noreply.github.com>
2026-07-28 20:48:13 -0300

    playerBallStates.cs & playerBall.cs actualizado

--- Assets/Scripts/gameplay/playerBall.cs
+++ Assets/Scripts/gameplay/playerBall.cs
@@ -10,46 +10,47 @@ public class playerBall : MonoBehaviour
     public Rigidbody2D rb2d;
     public GameObject reticle;
     public SpriteRenderer ballSprite;
+    public StateMachine stateMachine { get; private set; }
 
     [Header("Inputs")]
     public InputActionAsset inputs;
-    private InputAction inputShoot;
+    public InputAction inputShoot;
     private InputAction inputAimLeft;
     private InputAction inputAimRight;
 
     [Header("Properties")]
-    public float power;
-
-    private void OnEnable()
-    {
-        inputs.FindActionMap("Player").Enable();
-    }
+    public float charge = 1;
+    public float basePower = 50;
 
     private void Awake()
     {
+        stateMachine = new StateMachine();
+        stateMachine.ChangeState(new AimingState(this));
+
         inputShoot = InputSystem.actions.FindAction("Shoot");
         inputAimLeft = InputSystem.actions.FindAction("AimLeft");
         inputAimRight = InputSystem.actions.FindAction("AimRight");
     }
 
+    private void Start()
+    {
+        return;
+    }
+
     private void Update()
     {
-        aimBall();
-        if (inputShoot.WasPressedThisFrame())
-        {
-            shootBall();
-        }
+        stateMachine.Tick();
     }
 
-    private void shootBall()
+    public void ShootBall()
     {
-        Vector3 target = reticle.GetComponentInChildren<SpriteRenderer>().transform.position;
-        target = target - transform.position;
-        Debug.Log(target);
-        rb2d.AddForce(target * power);
+        Vector3 angle = reticle.GetComponentInChildren<SpriteRenderer>().transform.position;
+        angle = angle - transform.position;
+        angle = angle * basePower;
+        rb2d.AddForce(angle * charge);
     }
 
-    private void aimBall()
+    public void AimBall()
     {
         if (inputAimLeft.IsPressed() && !inputAimRight.IsPressed())
         {
@@ -60,8 +61,25 @@ public class playerBall : MonoBehaviour
             Vector3 newRotation = new Vector3(0, 0, -50);
             reticle.transform.Rotate(newRotation * Time.deltaTime);
         }
-        
     }
 
- 
+    public void ChargeShot()
+    {
+        bool reverse = false;
+        if (!reverse)
+        {
+            charge += 0.01f;
+            if (charge < 1)
+            {
+                reverse = true;
+            }
+        } else
+        {
+            charge -= 0.01f;
+            if (charge > 1)
+            {
+                reverse = false;
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/gameplay/playerBallStates.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TurnoffState: IState    
+{
+    private playerBall playerBall;
+
+    public TurnoffState(playerBall playerBall) { this.playerBall = playerBall; }
+    
+    public void Enter()
+    {
+        playerBall.inputs.FindActionMap("Player").Disable();
+        playerBall.reticle.SetActive(false);
+        playerBall.ballSprite.color = new Color(playerBall.ballSprite.color.r - 0.3f, playerBall.ballSprite.color.g - 0.3f, playerBall.ballSprite.color.b - 0.3f);
+    }
+
+    public void Tick()
+    {
+        return;
+        //turnSystem va sacar y poner la bola en este state
+    }
+
+    public void Exit()
+    {
+        playerBall.inputs.FindActionMap("Player").Enable();
+        playerBall.ballSprite.color = new Color(1, 1, 1);
+    }
+}
+
+public class AimingState: IState
+{
+    private playerBall playerBall;
+
+    public AimingState(playerBall playerBall) { this.playerBall = playerBall; }
+
+    public void Enter()
+    {
+        playerBall.reticle.SetActive(true);
+    }
+
+    public void Tick()
+    {
+        playerBall.AimBall();
+        if (playerBall.inputShoot.WasPressedThisFrame())
+        {
+            playerBall.stateMachine.ChangeState(new ChargingState(playerBall));
+        }
+    }
+
+    public void Exit()
+    {
+        playerBall.charge = 0;
+    }
+}
+
+public class ChargingState : IState
+{
+    private playerBall playerBall;
+
+    public ChargingState(playerBall playerBall) { this.playerBall = playerBall; }
+
+    public void Enter()
+    {
+        return;
+    }
+
+    public void Tick()
+    {
+        playerBall.ChargeShot();
+        if (playerBall.inputShoot.WasPressedThisFrame())
+        {
+            playerBall.stateMachine.ChangeState(new FiredState(playerBall));
+        }
+    }
+
+    public void Exit()
+    {
+        playerBall.reticle.SetActive(false);
+    }
+}
+
+public class FiredState : IState
+{
+    private playerBall playerBall;
+
+    private float timer = 0;
+
+    public FiredState(playerBall playerBall) { this.playerBall = playerBall; }
+
+    public void Enter()
+    {
+        playerBall.ShootBall();
+        timer = 3.1f;
+    }
+
+    public void Tick()
+    {
+        if (playerBall.rb2d.velocity.x == 0f && playerBall.rb2d.velocity.y == 0)
+        {
+            timer -= 1 * Time.deltaTime;
+            //Debug.Log(timer);
+        }
+
+        if (timer <= 0)
+        {
+            playerBall.stateMachine.ChangeState(new TurnoffState(playerBall));
+        }
+        
+    }
+
+    public void Exit()
+    {
+        return;
+    }
+}
