d15a9306928f00bb03a4bd530eaf29dbff0ed436
JonasGonzalezUnahur <166155934+JonasGonzalezUnahur@users.noreply.github.com>
2026-07-29 17:28:48 -0300

    turnSystem.cs

---
+++ Assets/Scripts/system/turnSystem.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnSystem : MonoBehaviour
+{
+    [Header("Properties")]
+    public List<GameObject> playerList = new List<GameObject>();
+    public int currentPlayerTurn;
+
+    public void GoToNextPlayer()
+    {
+        playerBall currentPlayer;
+        playerBall nextPlayer;
+        currentPlayer = playerList[currentPlayerTurn - 1].GetComponent<playerBall>(); //resto 1 al int porque el index de una lista empieza en 0
+        currentPlayer.stateMachine.ChangeState(new TurnoffState(currentPlayer));
+        currentPlayerTurn += 1;
+        if (currentPlayerTurn > playerList.Count)
+        {
+            currentPlayerTurn = 1;
+        }
+        nextPlayer = playerList[currentPlayerTurn - 1].GetComponent<playerBall>();
+        nextPlayer.stateMachine.ChangeState(new AimingState(nextPlayer));
+
+    }
+
+    public void ResetTurnOrder()
+    {
+        playerBall player;
+        currentPlayerTurn = 1;
+        player = playerList[currentPlayerTurn - 1].GetComponent<playerBall>();
+        player.stateMachine.ChangeState(new AimingState(player));
+    }
+}
