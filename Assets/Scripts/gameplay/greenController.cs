ffea27e7fe1bcaf4edd9811489b839ead6583782
JonasGonzalezUnahur <166155934+JonasGonzalezUnahur@users.noreply.github.com>
2026-07-30 20:14:18 -0300

    partes para crear cursos

---
+++ Assets/Scripts/gameplay/greenController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class greenController : MonoBehaviour
+{
+    [Header("Components")]
+    public SpriteRenderer sprite;
+    public GameObject arrow;
+    
+    [Header("Slide")]
+    public bool isSlide = false;
+    public float slideAngle;
+
+    public void Awake()
+    {
+        if (isSlide)
+        {
+            sprite.color = new Color(sprite.color.r - 0.3f, sprite.color.g - 0.3f, sprite.color.b - 0.3f);
+            arrow.SetActive(true);
+            arrow.transform.Rotate(0, 0, slideAngle);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        float slideAnglex = Mathf.Cos(slideAngle * Mathf.PI / 180);
+        float slideAngley = Mathf.Sin(slideAngle * Mathf.PI / 180);
+        if (collision.gameObject.CompareTag("Player") && isSlide)
+        {
+            var player = collision.GetComponent<playerBall>();
+            player.rb2d.AddForce(new Vector2(slideAnglex, slideAngley) * 2);
+        }
+    }
+}
---
+++ Assets/Scripts/gameplay/holeController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class holeController : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && collision.gameObject.activeSelf)
+        {
+            return;
+        }
+    }
+}
