ae8b2c4cfe572fbb49fed949640a553543271405
JonasGonzalezUnahur <166155934+JonasGonzalezUnahur@users.noreply.github.com>
2026-08-04 20:21:25 -0300

    cameraController actualizado

--- Assets/Scripts/system/cameraController.cs
+++ Assets/Scripts/system/cameraController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class cameraController : MonoBehaviour
 {
@@ -9,15 +10,80 @@ public class cameraController : MonoBehaviour
 
     [Header("image")]
     public GameObject target;
-    public int zoom;
+
+    [Header("Proporties")]
+    public float moveSpeed = 10f;
+    public bool freeroam = false;
+
+    [Header("Inputs")]
+    public InputActionAsset inputs;
+    private InputAction toggleMode;
+    private InputAction cameraLeft;
+    private InputAction cameraRight;
+    private InputAction cameraDown;
+    private InputAction cameraUp;
+
+    public void Awake()
+    {
+        toggleMode = InputSystem.actions.FindAction("ToggleCamera");
+        cameraLeft = InputSystem.actions.FindAction("CameraLeft");
+        cameraRight = InputSystem.actions.FindAction("CameraRight");
+        cameraDown = InputSystem.actions.FindAction("CameraDown");
+        cameraUp = InputSystem.actions.FindAction("CameraUp");
+
+        inputs.FindActionMap("Camera").Disable();
+    }
 
     public void Update()
     {
-        cameraObj.transform.position = target.transform.position;
+        if (toggleMode.WasPressedThisFrame())
+        {
+            ToggleFreeroam();
+        }
+        if (!freeroam)
+        {
+            cameraObj.transform.position = target.transform.position;
+        } else
+        {
+            if (cameraLeft.IsPressed())
+            {
+                cameraObj.transform.position -= new Vector3(moveSpeed, 0) * Time.deltaTime;
+            }
+            if (cameraRight.IsPressed())
+            {
+                cameraObj.transform.position += new Vector3(moveSpeed, 0) * Time.deltaTime;
+            }
+            if (cameraDown.IsPressed())
+            {
+                cameraObj.transform.position -= new Vector3(0, moveSpeed) * Time.deltaTime;
+            }
+            if (cameraUp.IsPressed())
+            {
+                cameraObj.transform.position += new Vector3(0, moveSpeed) * Time.deltaTime;
+            }
+        }
+        
     }
 
     public void Changetarget(GameObject newTarget)
     {
         target = newTarget;
+    }
+
+    public void ToggleFreeroam()
+    {
+        freeroam = !freeroam;
+        if (freeroam)
+        {
+            inputs.FindActionMap("Camera").Enable();
+            inputs.FindActionMap("Player").Disable();
+            toggleMode = InputSystem.actions.FindAction("ToggleCamera");
+        }
+        else
+        {
+            inputs.FindActionMap("Camera").Disable();
+            inputs.FindActionMap("Player").Enable();
+            toggleMode = InputSystem.actions.FindAction("ToggleCamera");
+        }
     }
 }
